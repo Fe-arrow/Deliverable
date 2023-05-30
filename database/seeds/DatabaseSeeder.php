b948c01ec9aad2453d7b2c13544f94a2cc38e88c
Fe-arrow <1120787@planer.kanazawa-it.ac.jp>
2023-05-30 17:12:57 +0900

    Seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            GroupSeeder::class,
+            UserSeeder::class,
+            CategorySeeder::class,
+            AnnouncementSeeder::class,
+            Purchase_goodSeeder::class,
+            ItemSeeder::class
+            ]);
     }
 }
